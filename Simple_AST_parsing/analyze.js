069155e9322ea0924e931610eaa1ac9fde94a7a8
sm67nono <manna.subhadeep@gmail.com>
2017-12-16 21:37:34 +0100

    Update analyze.js

--- Simple_AST_parsing/analyze.js
+++ Simple_AST_parsing/analyze.js
@@ -4,8 +4,12 @@ var fs = require('fs'),
     esprima = require('esprima');
 var estraverse = require('estraverse');
 
+//Store Function Declarations
+var functionRecursionList=[];
 
-var functionLists=[];
+//For functional operations Map,Reduce, Filter
+var functionListForOperations=[];
+
 
 //Manual traversing the AST
 function traverse(node, func) {
@@ -26,16 +30,19 @@ function traverse(node, func) {
     }
 }
 
-//Check for recursive pattern
+//Check for recursive pattern : smanna Dec 2, 2017
 function checkifRecursive()
 {
-for(var i = 0, len = functionLists.length; i < len; i++)
+for(var i = 0, len = functionRecursionList.length; i < len; i++)
   {
-    var currentfunctionAST = functionLists[i];
-    var functionName = currentfunctionAST.id.name;
+    var currentfunctionAST = functionRecursionList[i];
+    var functionName = '';
+    if(currentfunctionAST.type==="FunctionDeclaration")
+    {
+        functionName=currentfunctionAST.id.name;
+    }
     estraverse.traverse(currentfunctionAST, {
     enter: function(node){
-      //if (node.type === 'FunctionDeclaration' || node.type === 'FunctionExpression' || node.type === 'Program')
       if (node.type === 'CallExpression')
       {
         var calleefunctionName=node.callee.name;
@@ -49,10 +56,59 @@ for(var i = 0, len = functionLists.length; i < len; i++)
       }
     }
     });
-    //console.log(i+" function "+JSON.stringify(functionLists[i],null,4));
+    //console.log(i+" function "+JSON.stringify(functionRecursionList[i],null,4));
   }
 
 }
+
+
+//Check for functional pattern map, reduce and filter: smanna Dec 8, 2017
+function checkifFunctionalOperations()
+{
+for(var i = 0, len = functionListForOperations.length; i < len; i++)
+  {
+    var currentfunctionAST = functionListForOperations[i];
+    var functionName = 'Program';
+    if(currentfunctionAST.type==="FunctionDeclaration")
+    {
+        functionName=currentfunctionAST.id.name;
+    }
+    estraverse.traverse(currentfunctionAST, {
+    enter: function(node){
+      if (node.type === 'CallExpression')
+      {
+        //Check for Filter, Map and Reduce here
+
+        var typeOfOp='';
+        if(node.callee.property)
+        {
+          typeOfOp=node.callee.property.name;
+        }
+        //Prints out Filter functions
+        if(typeOfOp=='filter')
+        {
+                console.log(functionName+" is using a  filter operation.");
+
+        }
+        //Prints out Map functions
+        if(typeOfOp=='map')
+        {
+                console.log(functionName+" is using a  Map operation.");
+
+        }
+        //Prints out Reduce functions
+        if(typeOfOp=='reduce')
+        {
+                console.log(functionName+" is using a  Reduce operation.");
+
+        }
+      }
+    }
+    });
+  }
+
+}
+
 
 
 
@@ -60,10 +116,11 @@ for(var i = 0, len = functionLists.length; i < len; i++)
 function findRecursion(ast){
 estraverse.traverse(ast, {
 enter: function(node){
-  //if (node.type === 'FunctionDeclaration' || node.type === 'FunctionExpression' || node.type === 'Program')
-  if (node.type === 'FunctionDeclaration' || node.type === 'FunctionExpression')
+
+  
+  if (node.type === 'FunctionDeclaration' || node.type === 'FunctionExpression' || node.type === 'Program')
   {
-    functionLists.push(node);
+    functionRecursionList.push(node);
   }
 }
 });
@@ -73,25 +130,37 @@ checkifRecursive();
 }
 
 
+//Traversing using estraverse : smanna Dec 8, 2017
+function findfunctionalOps(ast){
+estraverse.traverse(ast, {
+enter: function(node){
+  if (node.type === 'FunctionDeclaration' || node.type === 'FunctionExpression' || node.type === 'Program')
+  {
+    functionListForOperations.push(node);
+  }
+}
+});
+//Check for functoinal patterns(Filter, Map and Reduce)
+checkifFunctionalOperations();
+
+}
 
 
-
-//Static Analysis calls Here
+//Static Analysis calls Here : smanna Dec 8, 2017
 function analyzeCode(code) {
     // 1
 	 var ast = esprima.parse(code);
 	 console.log("The AST is:");
-	 console.log(JSON.stringify(ast,null,4));
+	// console.log(JSON.stringify(ast,null,4));
 
 	console.log("Traversing the AST Manually");
 	/*
     traverse(ast, function(node) {
         console.log(node.type);
     }); */
-
-
   console.log("Traversing the AST using estraverse");
   findRecursion(ast);
+  findfunctionalOps(ast);
 
 }
 
