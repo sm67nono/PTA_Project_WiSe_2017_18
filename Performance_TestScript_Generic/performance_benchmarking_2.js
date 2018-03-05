ba0bc35df84cf7650157bfbd19ed6346519a1bd3
sm67nono <manna.subhadeep@gmail.com>
2018-03-05 02:47:50 +0100

    Update performance_benchmarking_2.js

--- Performance_TestScript_Generic/performance_benchmarking_2.js
+++ Performance_TestScript_Generic/performance_benchmarking_2.js
@@ -1,3 +1,4 @@
+//Forking a Synchronous child process for "npm test"
 var exec = require('child_process').execSync;
 var now=require('performance-now');
 var cmd = "npm test";
@@ -14,3 +15,12 @@ for(var i=0;i<10;i++)
 }
 var t1 = now();
 console.log("Time taken for 10 Test iterations ", (t1-t0).toFixed(3),"milliseconds");
+
+//Test Suite for 100 iterations
+/*var t0 = now();
+for(var i=0;i<100;i++)
+{
+	exec(cmd, options);
+}
+var t1 = now();
+console.log("Time taken for 10 Test iterations ", (t1-t0).toFixed(3),"milliseconds");*/
