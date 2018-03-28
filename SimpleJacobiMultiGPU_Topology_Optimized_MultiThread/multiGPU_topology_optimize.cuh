8e3658ede88b8f9e08c313a048a321943ae0ce5c
sm67nono <manna.subhadeep@gmail.com>
2018-03-29 01:38:55 +0200

    Removing accidental commit of another project.
    Signed-off-by: sm67nono <manna.subhadeep@gmail.com>

--- SimpleJacobiMultiGPU_Topology_Optimized_MultiThread/multiGPU_topology_optimize.cuh
+++
@@ -1,8 +0,0 @@
-#ifndef MULTIGPU_TOPOLOGY_OPTIMIZE_H
-#define MULTIGPU_TOPOLOGY_OPTIMIZE_H
-#include<map>
-
-
-std::map<int,int> outputLatencyMatrix(int numGPUs, bool p2p, int numberOfDevicesAlong_X, int numberoOfDevicesAlong_Y, int domainDecom_Dim);
-
-#endif
--- SimpleJacobiMultiGPU_Topology_Optimized_MultiThread/testMultiGPU_Jacobi2D_Decom.cuh
+++
@@ -1,6 +0,0 @@
-#ifndef TESTMULTI_GPU2D_JACOBI_DECOM_H
-#define TESTMULTI_GPU2D_JACOBI_DECOM_H
-
-int performJacobi_MultiGPU2D_Decom(unsigned int dim, unsigned int numJacobiIt, float* A0, float* A1, float* A2, float* A3, float* A4, float* rhs, float* x_in);
-
-#endif
